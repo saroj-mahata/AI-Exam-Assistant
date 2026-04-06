05166f273b3109d4f0d77dc94411a34544112228
saroj-mahata <sarojmahata9131@gmail.com>
2026-04-06 16:31:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI()
+
+UPLOAD_FOLDER = "data"
 
 @app.get("/")
 def home():
     return {"message": "AI Exam Assistant Running"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+        
+    return {"message": "File uploaded successfully"}
