1fb634bf53e24f2ad86658da7bed3e329e852a00
saroj-mahata <sarojmahata9131@gmail.com>
2026-04-06 16:37:13 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 import os
+import PyPDF2
 
 app = FastAPI()
 
@@ -9,6 +10,7 @@ UPLOAD_FOLDER = "data"
 def home():
     return {"message": "AI Exam Assistant Running"}
 
+
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
@@ -17,6 +19,27 @@ async def upload_file(file: UploadFile = File(...)):
         f.write(await file.read())
         
     return {"message": "File uploaded successfully"}
-    @app.post("/ask")
+
+
+def read_all_pdfs():
+    text = ""
+    
+    for file in os.listdir(UPLOAD_FOLDER):
+        if file.endswith(".pdf"):
+            file_path = os.path.join(UPLOAD_FOLDER, file)
+            with open(file_path, "rb") as f:
+                reader = PyPDF2.PdfReader(f)
+                for page in reader.pages:
+                    text += page.extract_text()
+    
+    return text
+
+
+@app.post("/ask")
 async def ask_question(question: str):
-    return {"answer": f"You asked: {question}"}
+    content = read_all_pdfs()
+
+    if question.lower() in content.lower():
+        return {"answer": "Answer found in your notes"}
+    
+    return {"answer": "I couldn't find answer in uploaded PDFs"}
